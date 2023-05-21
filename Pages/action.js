221c7902d79ddcca8d340e7aa7f43e9e15923847
Ahmed <ahmedabdlatif700@gmail.com>
2023-05-21 22:37:41 +0300

    first commit

---
+++ Pages/action.js
@@ -0,0 +1,15 @@
+    
+function showAllData() 
+    {
+     
+        window.location.href = "productsAll.html";
+
+    }
+
+    const icon = document.getElementsByClassName('.far');
+
+		icon.addEventListener('click', () => {
+			icon.style.color = 'black';
+		});
+
+
